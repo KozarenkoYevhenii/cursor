71e959ddbfa3a6ab8494d7d831cb009ed0d2b96c
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-01 18:29:47 +0300

    HW_12 done

--- HW_12/index.js
+++ HW_12/index.js
@@ -1,18 +1,24 @@
 const BASE_URL = "https://swapi.dev/api/";
+let filmNumber = 2;
 
 document.getElementById("getPersonages").addEventListener("click", () => {
-  getPersonagesInfo().then(renderPersonagesInfo);
+  getPersonagesInfo(filmNumber).then(renderPersonagesInfo);
 });
 
 document.getElementById("getPlanets").addEventListener("click", () => {
   getPlanets((page = 1)).then(renderPlanets);
 });
+document.getElementById("filmNumber").addEventListener("change", (event) => {
+  filmNumber = event.target.value;
+})
 
-async function getPersonagesInfo() {
-  const result = await axios(`${BASE_URL}films/2/`);
+async function getPersonagesInfo(filmNumber) {
+  const result = await axios(`${BASE_URL}films/${filmNumber}/`);
   const characters = [];
   for (let i = 0; i < result.data.characters.length; i++) {
-    let character = await axios(result.data.characters[i].replace("http", "https"));
+    let character = await axios(
+      result.data.characters[i].replace("http", "https")
+    );
     characters.push([
       character.data.name,
       character.data.birth_year,
@@ -23,8 +29,9 @@ async function getPersonagesInfo() {
 }
 
 function renderPersonagesInfo(characters = []) {
-  const container = document.getElementById("personages");
+  const container = document.getElementById("characters");
   container.innerHTML = "";
+  document.getElementById("planets").innerHTML = "";
 
   characters.forEach((char) => {
     const character = document.createElement("div");
@@ -32,7 +39,7 @@ function renderPersonagesInfo(characters = []) {
     character.innerHTML = `
     <h2>${char[0]}<h2>
     <h4>Birth year: ${char[1]}<h4>
-    <h4>Gender: ${char[1]}<h4>
+    <h4>Gender: ${char[2]}<h4>
     --------------------------
     `;
     container.append(character);
@@ -51,6 +58,7 @@ async function getPlanets(page) {
 function renderPlanets(planets = []) {
   const container = document.getElementById("planets");
   container.innerHTML = "";
+  document.getElementById("characters").innerHTML = "";
 
   planets.forEach((pl) => {
     const planet = document.createElement("div");
