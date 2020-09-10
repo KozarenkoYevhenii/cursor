ebfe357ff5e7dfc769e77d6cf42e172aa569c97a
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-10 21:35:47 +0300

    add dist folder

--- HW_14/webpack.config.js
+++ HW_14/webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  
   entry: "./src/index.js",
   devServer: {
     contentBase: "./",
