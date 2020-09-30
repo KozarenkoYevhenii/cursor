091300966a11ce8ed154e1335893fab69a5e3190
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-09-30 19:55:22 +0300

    add babel loader

--- HW_14/webpack.config.js
+++ HW_14/webpack.config.js
@@ -28,6 +28,16 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: ["file-loader"],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ],
   },
 };
